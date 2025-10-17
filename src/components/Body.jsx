bde973b3b42c784b5a72f0fe22a4210eb1a5cc8c
Shubham9454 <shubhamy.9454@gmail.com>
2025-10-17 18:54:08 +0530

    Created the body of website

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,34 +1,11 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import Navbar from "./test";
+import { Outlet} from "react-router-dom";
+import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { useEffect } from "react";
-import axios from "axios";
-import BASE_URL from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addUser } from "../utils/userSlice";
+
+
 
 const Body = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const userFromStore = useSelector((store) => store.user);
-
-  const fetchUser = async () => {
-    if (userFromStore) return;
-    try {
-      const res = await axios.get(BASE_URL + "/profile/view",
-        {withCredentials: true}
-      );
-      dispatch(addUser(res.data));
-    } catch (err) {
-      if (err.status === 401) navigate("/login");
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchUser();
-  }, []);
-
+  
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
