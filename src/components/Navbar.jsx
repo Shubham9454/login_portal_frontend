a0e3617dad1b8520a7f5cac9e2ade655d5abbede
Shubham9454 <shubhamy.9454@gmail.com>
2025-10-17 18:54:34 +0530

    Created the Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>{" "}
+          </svg>
+        </button>
+      </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="flex-none">
+        <Link
+            to="/login"
+            className="bg-gradient-to-r from-cyan-500 to-blue-600 text-white px-6 py-2 rounded-lg hover:from-cyan-600 hover:to-blue-700 transition-all transform hover:scale-105 font-medium shadow-lg"
+          >
+            Login/SignUp
+        </Link>
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>{" "}
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
