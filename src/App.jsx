8dae9fc861319789a323ba5c028af3e99198ab07
Shubham9454 <shubhamy.9454@gmail.com>
2025-10-17 18:53:11 +0530

    Added the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./pages/login";
 
 function App() {
-  
-
   return (
     <>
       <BrowserRouter basename="/">
-          <Routes>
-            <Route path="/" element={<Body />}>
-              <Route path="/" element={<SignUp />} />
-              <Route path="/login" element={<Login />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-        
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
