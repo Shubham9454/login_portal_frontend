b1b4ebb3dc3b5045fe8d143160706d3d2e0aafaf
Shubham9454 <shubhamy.9454@gmail.com>
2025-10-24 15:38:36 +0530

    created the user slice to add in store

---
+++ src/utils/userSlice.js
@@ -0,0 +1,19 @@
+
+
+const userSlice = createSlice({
+
+    name: "User",
+    initialState: null,
+    reducers: {
+        addUser: (state , action) =>{
+            return action.payload;
+        }, 
+        removeUser: (state , action) =>{
+            return null;
+        }
+    }
+});
+
+export const { addUser , removeUser} = userSlice.actions;
+
+export default userSlice.reducer;
