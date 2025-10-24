d0ab3c7717f6cb30da3184a05a1cbd49c9e3c3ab
Shubham9454 <shubhamy.9454@gmail.com>
2025-10-24 15:39:35 +0530

    added the store in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
-import Login from "./pages/login";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import store from "./utils/store";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
